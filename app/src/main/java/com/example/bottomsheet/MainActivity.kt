fd4a812731fe05695d71754345b491ebeb101703
Nooruz21 <nooruz21n@gmail.com>
2023-04-22 18:07:39 +0600

    first commit

---
+++ app/src/main/java/com/example/bottomsheet/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.bottomsheet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        BottomSheetBehavior.from(findViewById(R.id.sheet)).apply {
+            peekHeight = 200
+            this.state= BottomSheetBehavior.STATE_COLLAPSED
+        }
+    }
+}
